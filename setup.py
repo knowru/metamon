7c128f4f54731f1f197e3b48db5e5a6619974427
Ken Park <spark@knowru.com>
2018-02-03 10:53:09 +0900

    updated version from 0.1 to 0.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='metamon',
-    version='0.1',
+    version='0.1.1',
     description='Procuce metadata for your data',
     url='http://github.com/knowru/metamon',
     classifiers=[
